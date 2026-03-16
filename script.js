a604117acfab448154952f0b867fe50618b15f22
Enhoc Bedoya Garcia <bedoyaenhoc@gmail.com>
2026-03-16 10:25:06 -0500

    Actulizacion del Java Script

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 document.getElementById("btnCalcular").addEventListener("click", function(){
 
-    let edad = document.getElementById("edad").value;
-    let presion = document.getElementById("presion").value;
-    let colesterol = document.getElementById("colesterol").value;
+    let edad = parseInt(document.getElementById("edad").value);
+    let presion = parseInt(document.getElementById("presion").value);
+    let colesterol = parseInt(document.getElementById("colesterol").value);
     let fumador = document.getElementById("fumador").value;
 
     let textoResultado = document.getElementById("textoResultado");
 
-    if(edad === "" || presion === "" || colesterol === "" || fumador === ""){
-        
+    if(!edad || !presion || !colesterol || fumador === ""){
         textoResultado.textContent = "Por favor completa todos los campos antes de calcular.";
-
         return;
     }
 
-    console.log("Edad:", edad);
-    console.log("Presión:", presion);
-    console.log("Colesterol:", colesterol);
-    console.log("Fumador:", fumador);
+    let puntos = 0;
+
+    if(edad >= 35 && edad <= 44){
+        puntos += 1;
+    } 
+    else if(edad >= 45){
+        puntos += 2;
+    }
+
+    if(presion >= 140){
+        puntos += 3;
+    } 
+    else if(presion >= 120){
+        puntos += 1;
+    }
+
+    if(colesterol >= 240){
+        puntos += 3;
+    } 
+    else if(colesterol >= 200){
+        puntos += 1;
+    }
+
+    if(fumador === "si"){
+        puntos += 3;
+    }
+
+    console.log("Puntos totales:", puntos);
 
 });
